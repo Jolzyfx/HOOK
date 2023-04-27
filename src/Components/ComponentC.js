d0b76fa6210d16b13d06811ec9f67ffca075d449
JolzyFx Codes <jolzyanth@gmail.com>
2023-04-27 14:23:20 +0100

    hi

---
+++ src/Components/ComponentC.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComponentsE from './ComponentsE'
+
+function ComponentC() {
+  return (
+    <div>
+     <ComponentsE/>
+    </div>
+  )
+}
+
+export default ComponentC
